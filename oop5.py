d0402d019b7039d95edb4ebae7c3e9177ba5a2ef
Ashishraut812 <54107397+Ashishraut812@users.noreply.github.com>
2021-04-10 03:03:21 +0530

    5th and 6th oop program

---
+++ oop5.py
@@ -0,0 +1,13 @@
+# inside instance method by using self variable;
+
+class Test:
+    def __init__(self):
+        self.a=10
+        self.b=20
+
+    def m1(self):
+        self.c=30
+
+t=Test()
+t.m1()
+print(t.__dict__)
